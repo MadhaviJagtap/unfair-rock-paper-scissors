296acd0144b18b7b6a9ecff2957f74e7e7961062
Madhavi Jagtap <117476112+MadhaviJagtap@users.noreply.github.com>
2024-03-13 12:36:08 +0530

    Update unfair_game.py
    updated to break when invalid input

--- unfair_game.py
+++ unfair_game.py
@@ -2,37 +2,34 @@ import art as a
 
 print(a.logo)
 
-user = int(
-    input(
-        "What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"
-    ))
-comp = []
+while True:
+    user = int(input("What do you choose? Type 0 for Rock, 1 for Paper, or 2 for Scissors.\n"))
 
-if user == 0:
-    print("\nYou chose Rock!\n", a.rock)
-    comp = 1
-elif user == 1:
-    print("\nYou chose Paper!\n", a.paper)
-    comp = 2
-elif user == 2:
-    print("\nYou chose Scissors!\n", a.scissors)
-    comp = 0
-else:
-    print("Enter valid input")
+    if user not in [0, 1, 2]:
+        print("You haven't entered a valid input. Thanks for playing!")
+        break
 
-print("Computer chose:")
-if comp == 0:
-    print("\nRock!\n", a.rock)
-elif comp == 1:
-    print("\nPaper!\n", a.paper)
-else
-    print("\nScissors!\n", a.scissors)
+    if user == 0:
+        print("\nYou chose Rock!\n", a.rock)
+        comp = 1
+    elif user == 1:
+        print("\nYou chose Paper!\n", a.paper)
+        comp = 2
+    elif user == 2:
+        print("\nYou chose Scissors!\n", a.scissors)
+        comp = 0
 
-# rock 0
-# paper 1
-# scissors 2
+    print("Computer chose:")
+    if comp == 0:
+        print("\nRock!\n", a.rock)
+    elif comp == 1:
+        print("\nPaper!\n", a.paper)
+    else:
+        print("\nScissors!\n", a.scissors)
 
-if user != comp:
-    print("You lose!")
-else:
-    print("You win!")
+    if user != comp:
+        print("You lose!")
+        break
+    else:
+        print("You win!")
+        break
